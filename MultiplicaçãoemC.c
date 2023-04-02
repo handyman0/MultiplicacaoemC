1218047182b066314790f7c7402fc34a20502248
handyman0 <118775048+handyman0@users.noreply.github.com>
2023-04-02 15:42:32 -0300

    Add files via upload

---
+++ MultiplicaçãoemC.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// std = Standard (Padrão)
+// io = input/output (Entrada/Saída)
+
+int main(){
+	//Declaração de variáveis
+	int num1, num2, multiplicacao;
+
+	//Entradas
+	printf("Informe o primeiro número: ");
+	scanf("%d", &num1);
+
+	printf("Informe o segundo número: ");
+	scanf("%d", &num2);
+
+	//Processamento
+	multiplicacao = soma * num1;
+
+	//Saída
+	printf("O resultado da multiplicação é %d", multiplicacao);
+
+}
